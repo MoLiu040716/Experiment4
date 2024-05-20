a78d89f93d76b863b8a9a9a11280ca288aadf3e7
浪漫的飞猪 <12833318+romantic-flying-pig@user.noreply.gitee.com>
2024-05-20 14:19:46 +0800

    更改

---
+++ Provider_Service_11001/src/main/java/ynu/edu/controller/UserController.java
@@ -0,0 +1,32 @@
+package ynu.edu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ynu.edu.entity.CommonResult;
+import ynu.edu.entity.User;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/getUserById/{userId}")
+    public CommonResult<User> getUserById(@PathVariable("userId") Integer userId){
+        CommonResult<User> result = new CommonResult<>();
+        Integer code = 200;
+        String message = "success(11001)";
+        try {
+            User user = new User(userId, "zjh", "123");
+            result.setResult(user);
+
+        }catch (Exception e){
+            code=500;
+            message="failed";
+        }
+        result.setCode(code);
+        result.setMessage(message);
+        return result;
+    }
+
+}
